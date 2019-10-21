7c98b8bc554c2d8ae212b1cf1f78ad4aa53ad129
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 12:53:41 +0100

    11th test passing, dep AND with

--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -6,10 +6,16 @@ function BankAccount() {
 BankAccount.prototype.statement = function () {
   let all_debits = this.sort("debit")
   let all_credits = this.sort("credit")
+  let all_credit_values = []
+  let all_debit_values = []
+  all_credits.forEach(function (x) { all_credit_values.push(x[1]) })
+  all_debits.forEach(function (x) { all_debit_values.push(x[1]) })
+  let balance = all_credit_values - all_debit_values
+
   if (all_debits[0]) {
-    return `${this._header}21/10/2019 || || ${all_debits[0][1]}.00 || -1.00\n`
+    return `${this._header}21/10/2019 || || ${all_debits[0][1]}.00 || ${balance}.00\n`
   } else if (all_credits[0]) {
-    return `${this._header}21/10/2019 || ${all_credits[0][1]}.00 || || 1.00\n`
+    return `${this._header}21/10/2019 || ${all_credits[0][1]}.00 || || ${balance}.00\n`
   } else {
     return `${this._header}`
   }
