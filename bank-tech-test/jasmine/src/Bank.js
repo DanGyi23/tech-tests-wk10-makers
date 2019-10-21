2533a5eb1c6cd2e5f0c3ba1e834b93b9d987d189
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 12:10:11 +0100

    7th test passing, update to handle dep/with

--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -2,13 +2,22 @@ function BankAccount() {
   this._history = []
 }
 
-BankAccount.prototype.statement = function() {
-  return `date || credit || debit || balance\n21/10/2019 || ${this._history[0]}.00 || ||`
+BankAccount.prototype.statement = function () {
+  let all_debits = this._history.filter(function (x) { return x.includes("debit") });
+  let all_credits = this._history.filter(function (x) { return x.includes("credit") });
+  if (all_debits[0]) {
+    return `date || credit || debit || balance\n21/10/2019 || || ${all_debits[0][1]}.00 ||`
+  } else if (all_credits[0]) {
+    return `date || credit || debit || balance\n21/10/2019 || ${all_credits[0][1]}.00 || ||`
+  } else {
+    return `date || credit || debit || balance\n`
+  }
 }
 
-BankAccount.prototype.withdraw = function () {
+BankAccount.prototype.withdraw = function (amount) {
+  this._history.push(['debit', amount])
 }
 
 BankAccount.prototype.deposit = function (amount) {
-  this._history.push(amount)
+  this._history.push(['credit', amount])
 }
