618fd214f84e3c7ec94de74fd0d3afa520e65971
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 17:08:38 +0100

    refactor our sell_in update

--- gilded-rose/spec/gilded_rose_spec.rb
+++ gilded-rose/spec/gilded_rose_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require_relative '../gilded_rose'
+require_relative '../item'
 
 describe GildedRose do
-  describe '#update_quality' do
+  describe '#stock_update' do
     describe 'Non-Exceptional, non-conjured Items' do
       describe 'Standard Behaviour' do
         before(:each) do
@@ -12,37 +13,37 @@ describe GildedRose do
         end
 
         it 'Does not change the name on update quality' do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
           expect(@items[0].name).to eq 'Elixir of the Mongoose'
         end
 
         it 'Normal item quality decreases by 1 each day' do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
           expect(@items[0].quality).to eq 4
         end
 
         it 'Normal items SellIn decreases by 1 each day' do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
           expect(@items[0].sell_in).to eq 4
         end
 
         it 'Normal items quality decreases 3 times after 3 updates' do
           3.times do
-            @gilded_rose.update_quality
+            @gilded_rose.stock_update
           end
           expect(@items[0].quality).to eq 2
         end
 
         it 'Normal items SellIn decreases 3 times after 3 updates' do
           3.times do
-            @gilded_rose.update_quality
+            @gilded_rose.stock_update
           end
           expect(@items[0].sell_in).to eq 2
         end
 
         it 'Normal item quality decreases from 4 to 0 ONLY, cannot be negative' do
           6.times do
-            @gilded_rose.update_quality
+            @gilded_rose.stock_update
           end
           expect(@items[0].quality).to eq 0
         end
@@ -55,14 +56,14 @@ describe GildedRose do
         end
         it 'Normal item quality decreases by 2 after sell_in past' do
           2.times do
-            @gilded_rose.update_quality
+            @gilded_rose.stock_update
           end
           expect(@items[0].quality).to eq 7
         end
 
         it 'Normal item quality decreases by 2 for each day past the sell_in' do
           5.times do
-            @gilded_rose.update_quality
+            @gilded_rose.stock_update
           end
           expect(@items[0].quality).to eq 1
         end
@@ -75,19 +76,19 @@ describe GildedRose do
       end
 
       it 'Brie quality increases by 1 after 1 day' do
-        @gilded_rose.update_quality
+        @gilded_rose.stock_update
         expect(@items[0].quality).to eq 6
       end
 
       it 'Brie quality increases doubly after sell_in date passed' do
         6.times do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
         end
         expect(@items[0].quality).to eq 12
       end
 
       it 'Brie sell_in decreases as normal' do
-        @gilded_rose.update_quality
+        @gilded_rose.stock_update
         expect(@items[0].sell_in).to eq 4
       end
     end
@@ -99,7 +100,7 @@ describe GildedRose do
       end
       it 'Does not change in quality or sell_in' do
         6.times do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
         end
         expect(@items[0].sell_in).to eq 5
         expect(@items[0].quality).to eq 5
@@ -113,53 +114,54 @@ describe GildedRose do
       end
 
       it 'Increases in quality by 1 after 1 day if sell_in > 10' do
-        @gilded_rose.update_quality
+        @gilded_rose.stock_update
         expect(@items[0].quality).to eq 6
       end
 
       it 'Decreases sell_in as normal' do
-        @gilded_rose.update_quality
+        @gilded_rose.stock_update
         expect(@items[0].sell_in).to eq 11
       end
 
       it 'Increases in quality by 2 if 11 > sell_in > 5' do
-        3.times do
-          @gilded_rose.update_quality
+        2.times do
+          @gilded_rose.stock_update
         end
-        expect(@items[0].quality).to eq 9
+        expect(@items[0].quality).to eq 8
       end
 
       it 'Increases in quality by 3 if 5 > sell_in > 0' do
         7.times do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
         end
-        expect(@items[0].quality).to eq 17
-        @gilded_rose.update_quality
-        expect(@items[0].quality).to eq 20
+        p @items[0].sell_in
+        expect(@items[0].quality).to eq 19
+        @gilded_rose.stock_update
+        expect(@items[0].quality).to eq 22
       end
 
       it 'Quality drops to zero when sell_in date is negative' do
         12.times do
-          @gilded_rose.update_quality
+          @gilded_rose.stock_update
         end
-        expect(@items[0].quality).to eq 32
-        @gilded_rose.update_quality
+        expect(@items[0].quality).to eq 34
+        @gilded_rose.stock_update
         expect(@items[0].quality).to eq 0
       end
     end
 
     describe 'Conjured Items' do
-      describe 'Normal conjured Items' do
-        before(:each) do
-            @items = [Item.new('Conjured Elixir of the Mongoose', 5, 5)]
-            @gilded_rose = GildedRose.new(@items)
-          end
-        it 'Normal, conjured item. Drops in quality by 2 each day' do
+      # describe 'Normal conjured Items' do
+      #   before(:each) do
+      #       @items = [Item.new('Conjured Elixir of the Mongoose', 5, 5)]
+      #       @gilded_rose = GildedRose.new(@items)
+      #     end
+      #   it 'Normal, conjured item. Drops in quality by 2 each day' do
           
-          @gilded_rose.update_quality
-          expect(@items[0].quality).to eq 3
-        end
-      end
+      #     @gilded_rose.stock_update
+      #     expect(@items[0].quality).to eq 3
+      #   end
+      # end
       # describe 'Brie, conjured' do
 
       # end
