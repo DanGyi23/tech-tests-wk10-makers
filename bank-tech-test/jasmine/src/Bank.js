cd61371f361cf6c1215a37e20789ea2e74432c4f
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 13:05:20 +0100

    refactor balance to new function

--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -6,12 +6,7 @@ function BankAccount() {
 BankAccount.prototype.statement = function () {
   let all_debits = this.sort("debit")
   let all_credits = this.sort("credit")
-  let all_credit_values = []
-  let all_debit_values = []
-  all_credits.forEach(function (x) { all_credit_values.push(x[1]) })
-  all_debits.forEach(function (x) { all_debit_values.push(x[1]) })
-  let balance = all_credit_values - all_debit_values
-
+  let balance = this.balanceCalculator();
   if (all_debits[0]) {
     return `${this._header}21/10/2019 || || ${all_debits[0][1]}.00 || ${balance}.00\n`
   } else if (all_credits[0]) {
@@ -31,4 +26,14 @@ BankAccount.prototype.deposit = function (amount) {
 
 BankAccount.prototype.sort = function (type) {
   return this._history.filter(function (x) { return x.includes(type) });
+}
+
+BankAccount.prototype.balanceCalculator = function () {
+  let all_debits = this.sort("debit")
+  let all_credits = this.sort("credit")
+  let all_credit_values = []
+  let all_debit_values = []
+  all_credits.forEach(function (x) { all_credit_values.push(x[1]) })
+  all_debits.forEach(function (x) { all_debit_values.push(x[1]) })
+  return all_credit_values - all_debit_values
 }
