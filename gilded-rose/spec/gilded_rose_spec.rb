0a07861cd0c2b97764ef035e631fe00dcbdc6c43
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 16:39:43 +0100

    Backstage Pass tests written

--- gilded-rose/spec/gilded_rose_spec.rb
+++ gilded-rose/spec/gilded_rose_spec.rb
@@ -81,9 +81,65 @@ describe GildedRose do
 
       it 'Brie quality increases doubly after sell_in date passed' do
         6.times do
-            @gilded_rose.update_quality
-          end
+          @gilded_rose.update_quality
+        end
         expect(@items[0].quality).to eq 12
+      end
+
+      it 'Brie sell_in decreases as normal' do
+        @gilded_rose.update_quality
+        expect(@items[0].sell_in).to eq 4
+      end
+    end
+
+    describe 'Sulfuras, non-conjured' do
+      before(:each) do
+        @items = [Item.new('Sulfuras, Hand of Ragnaros', 5, 5)]
+        @gilded_rose = GildedRose.new(@items)
+      end
+      it 'Does not change in quality or sell_in' do
+        6.times do
+          @gilded_rose.update_quality
+        end
+        expect(@items[0].sell_in).to eq 5
+        expect(@items[0].quality).to eq 5
+      end
+    end
+
+    describe 'Backstage Passes, non-conjured' do
+      before(:each) do
+        @items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 5)]
+        @gilded_rose = GildedRose.new(@items)
+      end
+
+      it 'Increases in quality by 1 after 1 day if sell_in > 10' do
+        @gilded_rose.update_quality
+        expect(@items[0].quality).to eq 6
+      end
+
+      it 'Increases in quality by 2 if 11 > sell_in > 5' do
+        3.times do
+          @gilded_rose.update_quality
+        end
+        expect(@items[0].quality).to eq 9
+      end
+
+      it 'Increases in quality by 3 if 5 > sell_in > 0' do
+        7.times do
+          @gilded_rose.update_quality
+        end
+        expect(@items[0].quality).to eq 17
+        @gilded_rose.update_quality
+        expect(@items[0].quality).to eq 20
+      end
+
+      it 'Quality drops to zero when sell_in date is negative' do
+        12.times do
+          @gilded_rose.update_quality
+        end
+        expect(@items[0].quality).to eq 32
+        @gilded_rose.update_quality
+        expect(@items[0].quality).to eq 0
       end
     end
   end
