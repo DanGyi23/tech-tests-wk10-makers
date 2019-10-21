626f6788adbdbfac5daf32c6fbffb550ae9958ad
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 17:23:08 +0100

    refactored different logic to diff methods

--- gilded-rose/gilded_rose.rb
+++ gilded-rose/gilded_rose.rb
@@ -9,55 +9,56 @@ class GildedRose
 
   def stock_update
     update_sell_in
-    # update_quality_brie
-    update_quality
+    update_quality_brie
+    update_quality_backstage_pass
+    # update_normal_quality
   end
 
   
 
-  def update_quality
-    @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else # normal items
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-            if item.sell_in < 6
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-          end
-        end
-      end
+  # def update_quality
+  #   @items.each do |item|
+  #     if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+  #       if item.quality > 0
+  #         if item.name != 'Sulfuras, Hand of Ragnaros'
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else # normal items
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.sell_in < 11
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #           if item.sell_in < 6
+  #             item.quality = item.quality + 1 if item.quality < 50
+  #           end
+  #         end
+  #       end
+  #     end
 
-      # if item.name != "Sulfuras, Hand of Ragnaros"
-      #   item.sell_in = item.sell_in - 1
-      # end
+  #     # if item.name != "Sulfuras, Hand of Ragnaros"
+  #     #   item.sell_in = item.sell_in - 1
+  #     # end
 
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          item.quality = item.quality + 1 if item.quality < 50
-        end
-      end
-    end
-  end
+  #     if item.sell_in < 0
+  #       if item.name != 'Aged Brie'
+  #         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+  #           if item.quality > 0
+  #             if item.name != 'Sulfuras, Hand of Ragnaros'
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         item.quality = item.quality + 1 if item.quality < 50
+  #       end
+  #     end
+  #   end
+  # end
 
   private
 
@@ -80,4 +81,21 @@ class GildedRose
       end
     end
   end
+
+  def update_quality_backstage_pass
+    @items.each do |item|
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
+        if item.sell_in > 10
+          item.quality += 1
+        elsif item.sell_in <= 10 && item.sell_in > 5
+          item.quality += 2
+        elsif item.sell_in <= 5 && item.sell_in >= 0
+          item.quality += 3
+        else
+          item.quality = 0
+        end
+      end
+    end
+  end
+
 end
