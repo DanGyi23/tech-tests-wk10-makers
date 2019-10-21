092394bb76804d68051330c0ae3761536df196ed
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 11:09:13 +0100

    first test written and failing

---
+++ bank-tech-test/jasmine/spec/bankSpec.js
@@ -0,0 +1,14 @@
+describe("Bank", function() {
+  var account;
+
+  beforeEach(function() {
+    account = new Account();
+  });
+
+  it("Perform ANY ACTION with ANY amount and the column headers are returned with a newline", function() {
+    account.withdraw(10);
+    expect(account.statement).toContain('date || credit || debit || balance\n');
+  });
+
+  
+});
