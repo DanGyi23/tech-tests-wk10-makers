e1b4786170d26ff561aa212a9197cb12a51d73ee
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 15:27:24 +0100

    refactor tests

--- gilded-rose/spec/gilded_rose_spec.rb
+++ gilded-rose/spec/gilded_rose_spec.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require_relative '../gilded_rose'
 
 describe GildedRose do
+  describe '#update_quality' do
+    describe 'Non-Exceptional Items' do
+      before(:each) do
+        @items = [Item.new('Elixir of the Mongoose', 2, 50)]
+      end
+      it 'does not change the name on update quality' do
+        GildedRose.new(@items).update_quality
+        expect(@items[0].name).to eq 'Elixir of the Mongoose'
+      end
 
-  
-  describe "#update_quality" do
-    it "does not change the name on quality update" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-    end
+      it 'Normal item quality decreases by 1 each day' do
+        GildedRose.new(@items).update_quality
+        expect(@items[0].quality).to eq 49
+      end
 
-    it "Normal item quality decreases by 1 each day" do
-      items = [Item.new("Elixir of the Mongoose", 2, 50)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq 49
-    end
-
-    it "Normal items SellIn decreases by 1 each day" do
-      items = [Item.new("Elixir of the Mongoose", 2, 50)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq 1
+      it 'Normal items SellIn decreases by 1 each day' do
+        GildedRose.new(@items).update_quality
+        expect(@items[0].sell_in).to eq 1
+      end
     end
   end
-
 end
