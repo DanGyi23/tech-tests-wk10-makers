6d062617369193dab647974b010a9a82b3fb3e54
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 16:18:06 +0100

    More tests passing for normal items

--- gilded-rose/spec/gilded_rose_spec.rb
+++ gilded-rose/spec/gilded_rose_spec.rb
@@ -5,45 +5,69 @@ require_relative '../gilded_rose'
 describe GildedRose do
   describe '#update_quality' do
     describe 'Non-Exceptional, non-conjured Items' do
-      before(:each) do
-        @items = [Item.new('Elixir of the Mongoose', 5, 5)]
-        @gilded_rose = GildedRose.new(@items)
-      end
-      it 'Does not change the name on update quality' do
-        @gilded_rose.update_quality
-        expect(@items[0].name).to eq 'Elixir of the Mongoose'
-      end
-
-      it 'Normal item quality decreases by 1 each day' do
-        @gilded_rose.update_quality
-        expect(@items[0].quality).to eq 4
-      end
-
-      it 'Normal items SellIn decreases by 1 each day' do
-        @gilded_rose.update_quality
-        expect(@items[0].sell_in).to eq 4
-      end
-
-      it 'Normal items quality decreases 3 times after 3 updates' do
-        3.times do
-          @gilded_rose.update_quality
+      describe 'Standard Behaviour' do
+        before(:each) do
+          @items = [Item.new('Elixir of the Mongoose', 5, 5)]
+          @gilded_rose = GildedRose.new(@items)
         end
-        expect(@items[0].quality).to eq 2
+
+        it 'Does not change the name on update quality' do
+          @gilded_rose.update_quality
+          expect(@items[0].name).to eq 'Elixir of the Mongoose'
+        end
+
+        it 'Normal item quality decreases by 1 each day' do
+          @gilded_rose.update_quality
+          expect(@items[0].quality).to eq 4
+        end
+
+        it 'Normal items SellIn decreases by 1 each day' do
+          @gilded_rose.update_quality
+          expect(@items[0].sell_in).to eq 4
+        end
+
+        it 'Normal items quality decreases 3 times after 3 updates' do
+          3.times do
+            @gilded_rose.update_quality
+          end
+          expect(@items[0].quality).to eq 2
+        end
+
+        it 'Normal items SellIn decreases 3 times after 3 updates' do
+          3.times do
+            @gilded_rose.update_quality
+          end
+          expect(@items[0].sell_in).to eq 2
+        end
+
+        it 'Normal item quality decreases from 4 to 0 ONLY, cannot be negative' do
+          6.times do
+            @gilded_rose.update_quality
+          end
+          expect(@items[0].quality).to eq 0
+        end
       end
 
-      it 'Normal items SellIn decreases 3 times after 3 updates' do
-        3.times do
-          @gilded_rose.update_quality
+      describe 'Non-Standard Behaviour' do
+        before(:each) do
+          @items = [Item.new('Elixir of the Mongoose', 1, 10)]
+          @gilded_rose = GildedRose.new(@items)
         end
-        expect(@items[0].sell_in).to eq 2
+        it 'Normal item quality decreases by 2 after sell_in past' do
+          2.times do
+            @gilded_rose.update_quality
+          end
+          expect(@items[0].quality).to eq 7
+        end
+
+        it 'Normal item quality decreases by 2 for each day past the sell_in' do
+          5.times do
+            @gilded_rose.update_quality
+          end
+          expect(@items[0].quality).to eq 1
+        end
       end
 
-      it 'Normal item quality decreases from 4 to 0 ONLY after 6 updates' do
-        6.times do
-          @gilded_rose.update_quality
-        end
-        expect(@items[0].quality).to eq 0
-      end
     end
   end
 end
