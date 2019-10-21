408fecd1beb3bc849fde34d051e1b0a7855dff28
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 13:32:00 +0100

    refactor to extract lines of statement

--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -4,19 +4,7 @@ function BankAccount() {
 }
 
 BankAccount.prototype.statement = function () {
-  let statement_array = []
-  let running_balance = 0
-  this._history.forEach(function(x) {
-    if (x.includes('credit')) {
-      running_balance += x[1]
-      statement_array.push(`21/10/2019 || ${x[1]}.00 || || ${running_balance}.00\n`)
-    } else if (x.includes('debit')) {
-      running_balance -= x[1]
-      statement_array.push(`21/10/2019 || || ${x[1]}.00 || ${running_balance}.00\n`)
-    }
-  });
-
-return this._header + statement_array.join('')
+return this._header + this.statementLineCreator();
 }
 
 BankAccount.prototype.withdraw = function (amount) {
@@ -31,12 +19,18 @@ BankAccount.prototype.sort = function (type) {
   return this._history.filter(function (x) { return x.includes(type) });
 }
 
-BankAccount.prototype.balanceCalculator = function () {
-  let all_debits = this.sort("debit")
-  let all_credits = this.sort("credit")
-  let all_credit_values = []
-  let all_debit_values = []
-  all_credits.forEach(function (x) { all_credit_values.push(x[1]) })
-  all_debits.forEach(function (x) { all_debit_values.push(x[1]) })
-  return all_credit_values - all_debit_values
+BankAccount.prototype.statementLineCreator = function () {
+  let statement_array = []
+  let running_balance = 0
+  this._history.forEach(function (x) {
+    if (x.includes('credit')) {
+      running_balance += x[1]
+      statement_array.push(`21/10/2019 || ${x[1]}.00 || || ${running_balance}.00\n`)
+    } else if (x.includes('debit')) {
+      running_balance -= x[1]
+      statement_array.push(`21/10/2019 || || ${x[1]}.00 || ${running_balance}.00\n`)
+    }
+  });
+
+  return statement_array.join('')
 }
