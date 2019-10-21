29c5ae99b3bb1db3e30407f02409cae74203751c
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 15:20:55 +0100

    test sell_in decreases

--- gilded-rose/spec/gilded_rose_spec.rb
+++ gilded-rose/spec/gilded_rose_spec.rb
@@ -4,16 +4,22 @@ describe GildedRose do
 
   
   describe "#update_quality" do
-    it "does not change the name" do
+    it "does not change the name on quality update" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
 
-    it "Normal items quality decrease by 1 each day" do
+    it "Normal item quality decreases by 1 each day" do
       items = [Item.new("Elixir of the Mongoose", 2, 50)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 49
+    end
+
+    it "Normal items SellIn decreases by 1 each day" do
+      items = [Item.new("Elixir of the Mongoose", 2, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 1
     end
   end
 
