cd0cd8e9c1e4cbb3a7f853a78551aa634055f372
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 11:38:47 +0100

    6th pass, using variable deposit amnt

--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -1,12 +1,14 @@
 function BankAccount() {
+  this._history = []
 }
 
 BankAccount.prototype.statement = function() {
-  return 'date || credit || debit || balance\n21/10/2019 || 1.00 || ||'
+  return `date || credit || debit || balance\n21/10/2019 || ${this._history[0]}.00 || ||`
 }
 
 BankAccount.prototype.withdraw = function () {
 }
 
-BankAccount.prototype.deposit = function () {
+BankAccount.prototype.deposit = function (amount) {
+  this._history.push(amount)
 }
