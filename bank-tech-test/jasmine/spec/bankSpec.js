558494aef17a151a242fcc9059a17767e18967e1
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 11:13:50 +0100

    first test passing, statement prints headers

--- bank-tech-test/jasmine/spec/bankSpec.js
+++ bank-tech-test/jasmine/spec/bankSpec.js
@@ -2,12 +2,12 @@ describe("Bank", function() {
   var account;
 
   beforeEach(function() {
-    account = new Account();
+    account = new BankAccount();
   });
 
   it("Perform ANY ACTION with ANY amount and the column headers are returned with a newline", function() {
     account.withdraw(10);
-    expect(account.statement).toContain('date || credit || debit || balance\n');
+    expect(account.statement()).toContain('date || credit || debit || balance\n');
   });
 
   
---
+++ bank-tech-test/jasmine/src/Bank.js
@@ -0,0 +1,9 @@
+function BankAccount() {
+}
+
+BankAccount.prototype.withdraw = function() {
+}
+
+BankAccount.prototype.statement = function() {
+  return 'date || credit || debit || balance\n'
+}
