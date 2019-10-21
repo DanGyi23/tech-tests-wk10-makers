bf7710033d1bf06a787a5c163137c22a25076026
Dan Gyi <daniel.gyi@gmail.com>
2019-10-21 11:21:10 +0100

    4th test failing, deposit function added

--- bank-tech-test/jasmine/spec/bankSpec.js
+++ bank-tech-test/jasmine/spec/bankSpec.js
@@ -5,20 +5,23 @@ describe("Bank", function() {
     account = new BankAccount();
   });
 
-  it("Perform ANY ACTION with ANY amount and the column headers are returned with a newline", function() {
+  it("Performs ANY ACTION with ANY amount and the column headers are returned with a newline", function() {
     account.withdraw(10);
     expect(account.statement()).toContain('date || credit || debit || balance\n');
   });
 
-  it("Perform ANY ACTION with ANY amount and the correct date is included in the string", function() {
+  it("Performs ANY ACTION with ANY amount and the correct date is included in the string", function() {
     account.withdraw(10)
     expect(account.statement()).toContain('21/10/2019')
   });
 
-  it('Perform ANY ACTION with ANY amount and the correct date is included in the string WITH the column headers', function(){
+  it('Performs ANY ACTION with ANY amount and the correct date is included in the string WITH the column headers', function(){
     account.withdraw(5)
     expect(account.statement()).toEqual('date || credit || debit || balance\n21/10/2019')
   });
 
-  
+  it('Performs DEPOSIT with 1 and credit field is populated with 1.00', function() {
+    account.deposit(1)
+    expect(account.statement()).toEqual('date || credit || debit || balance\n21/10/2019 || 1.00 ')
+  });
 });
--- bank-tech-test/jasmine/src/Bank.js
+++ bank-tech-test/jasmine/src/Bank.js
@@ -4,6 +4,9 @@ function BankAccount() {
 BankAccount.prototype.withdraw = function() {
 }
 
+BankAccount.prototype.deposit = function() {
+}
+
 BankAccount.prototype.statement = function() {
   return 'date || credit || debit || balance\n21/10/2019'
 }
