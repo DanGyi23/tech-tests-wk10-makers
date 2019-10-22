ab770e7c60e54a4141dc836ed67b29c22fa69bbb
Dan Gyi <daniel.gyi@gmail.com>
2019-10-22 11:15:58 +0100

    first conjured pass + refactor

--- gilded-rose/gilded_rose.rb
+++ gilded-rose/gilded_rose.rb
@@ -6,60 +6,68 @@ class GildedRose
   def initialize(items)
     @items = items
   end
+  
 
   def stock_update
-    update_sell_in
-    update_quality_backstage_pass
-    update_quality_brie
-    update_quality_normal
+    @items.each do |item|
+      if conjured?(item)
+        update_sell_in(item)
+        2.times do 
+          update_quality_backstage_pass(item)
+          update_quality_brie(item)
+          update_quality_normal(item)
+        end
+      else
+        update_sell_in(item)
+        update_quality_backstage_pass(item)
+        update_quality_brie(item)
+        update_quality_normal(item)
+      end
+    end
   end
 
   private
 
-  def update_sell_in
-    @items.each do |item|
+  def conjured?(item)
+    item.name.include?("Conjured") || item.name.include?("conjured")
+  end
+
+  def update_sell_in(item)
       unless item.name == 'Sulfuras, Hand of Ragnaros'
         item.sell_in -= 1
+    end
+  end
+
+  def update_quality_normal(item)
+    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.name != 'Sulfuras, Hand of Ragnaros') && item.quality > QUALITY_MIN
+      if item.sell_in >= 0
+        item.quality -= 1
+      else
+        item.quality -= 2
       end
     end
   end
 
-  def update_quality_normal
-    @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert') && (item.name != 'Sulfuras, Hand of Ragnaros') && item.quality > QUALITY_MIN
-        if item.sell_in >= 0
-          item.quality -= 1
-        else
-          item.quality -= 2
-        end
+  def update_quality_brie(item)
+    if item.name == 'Aged Brie' && item.quality < 50
+      if item.sell_in >= 0
+        item.quality += 1
+      else
+        item.quality += 2
       end
     end
   end
 
-  def update_quality_brie
-    @items.each do |item|
-      if item.name == 'Aged Brie' && item.quality < 50
-        if item.sell_in >= 0
-          item.quality += 1
-        else
-          item.quality += 2
-        end
-      end
-    end
-  end
-
-  def update_quality_backstage_pass
-    @items.each do |item|
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
-        if item.sell_in >= 10
-          item.quality += 1
-        elsif item.sell_in < 10 && item.sell_in >= 5
-          item.quality += 2
-        elsif item.sell_in < 5 && item.sell_in >= 0
-          item.quality += 3
-        else
-          item.quality = QUALITY_MIN
-        end
+  def update_quality_backstage_pass(item)
+    if item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.quality < 50
+      if item.sell_in >= 10
+        item.quality += 1
+      elsif item.sell_in < 10 && item.sell_in >= 5
+        item.quality += 2
+      elsif item.sell_in < 5 && item.sell_in >= 0
+        item.quality += 3
+      else
+        item.quality = QUALITY_MIN
       end
     end
   end
