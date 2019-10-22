caeb469214ee90455d25c776b472b15ebf4d3f72
Dan Gyi <daniel.gyi@gmail.com>
2019-10-22 13:29:55 +0100

    Item tests updated for coverage

---
+++ gilded-rose/spec/item_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../item'
+
+describe Item do
+  before(:each) do
+    @item = Item.new("Cool Item", 5, 8)
+  end
+  it 'is instantiated with name' do
+    expect(@item.name).to eq "Cool Item"
+  end
+  it 'is instantiated with sell_in' do
+    expect(@item.sell_in).to eq 5
+  end
+  it 'is instantiated with quality' do
+    expect(@item.quality).to eq 8
+  end
+
+  describe '#to_s' do
+    it 'returns the item properties as a string' do
+      expect(@item.to_s).to eq "Cool Item, 5, 8"
+    end
+  end
+end
