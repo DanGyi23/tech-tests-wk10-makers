ac6d05330d2fb179f96f5a620d4f48848486b33a
Dan Gyi <daniel.gyi@gmail.com>
2019-10-23 14:18:20 +0100

    row tests pass, need refactor

--- tic-tac-toe/jasmine/src/Game.js
+++ tic-tac-toe/jasmine/src/Game.js
@@ -2,10 +2,10 @@ function Game() {
   this._player1 = new Player(name= "X");
   this._player2 = new Player(name= "O");
   this._currentTurn = this._player1
-  this._board = new Array(9);
+  this._board = new Array(9)
 
   Game.prototype.makeTurn = function(index) {
-    if (this._board[index] === undefined) {
+    if (this._board[index] == undefined) {
     this._board[index] = this._currentTurn._name
     this.turnChanger();
     } else {
@@ -22,11 +22,11 @@ function Game() {
   };
 
   Game.prototype.winCheck = function() {
-    if (this._board[0] === this._board[1] && this._board[0] === this._board[2] && this._board[0] !== undefined) {
+    if (this._board[0] === this._board[1] && this._board[0] === this._board[2] && this._board[2] !== undefined) {
       return this._player1
-    } else if (this._board[3] === this._board[4] && this._board[3] === this._board[5] && this._board[3] !== undefined) {
+    } else if (this._board[3] === this._board[4] && this._board[3] === this._board[5] && this._board[5] !== undefined) {
       return this._player1
-    } else if (this._board[3] === this._board[4] && this._board[3] === this._board[5] && this._board[3] !== undefined) {
+    } else if (this._board[6] === this._board[7] && this._board[6] === this._board[8] && this._board[8] !== undefined) {
       return this._player1
     };
   };
