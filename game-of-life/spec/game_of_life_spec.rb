dcea72cf7d204cef3dc7d2aab4b5996fbfe9863d
Dan Gyi <daniel.gyi@gmail.com>
2019-10-25 14:10:08 +0100

    refactor tests

--- game-of-life/spec/game_of_life_spec.rb
+++ game-of-life/spec/game_of_life_spec.rb
@@ -1,23 +1,25 @@
 require 'game_of_life'
 
 describe GameOfLife do
+  before(:each) do
+      @game = GameOfLife.new
+    end
   describe '#tick' do
+    
     it 'responds to the method tick' do
-      expect(subject).to respond_to('tick')
+      expect(@game).to respond_to('tick')
     end
 
     it 'returns an array' do
-      expect(subject.tick).to be_kind_of Array
+      expect(@game.tick).to be_kind_of Array
     end
 
     it 'initializes with an array that reads continuously' do
-      @game = GameOfLife.new
       @game.board_array[1] = "read me"
       expect(@game.get_element_at(@game.board_array, (99 + 2))).to eq("read me")
     end
 
-     it 'initializes with an array that reads continuously' do
-      @game = GameOfLife.new
+    it 'initializes with an array that reads continuously' do
       @game.board_array[4] = "read me"
       expect(@game.get_element_at(@game.board_array, (99 + 5))).to eq("read me")
     end
