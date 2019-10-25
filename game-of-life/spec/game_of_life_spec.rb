488a4ff6d2c6e095447d12397759b1a25ad15fa8
Dan Gyi <daniel.gyi@gmail.com>
2019-10-25 13:57:16 +0100

    2nd test failing

--- game-of-life/spec/game_of_life_spec.rb
+++ game-of-life/spec/game_of_life_spec.rb
@@ -5,5 +5,9 @@ describe GameOfLife do
     it 'responds to the method tick' do
       expect(subject).to respond_to('tick')
     end
+
+    it 'returns an array' do
+      expect(subject.tick).to be_kind_of Array
+    end
   end
 end
