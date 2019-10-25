334726d558f39d197acf0d68d506b258b14e9462
Dan Gyi <daniel.gyi@gmail.com>
2019-10-25 14:01:47 +0100

    3rd test fails

--- game-of-life/spec/game_of_life_spec.rb
+++ game-of-life/spec/game_of_life_spec.rb
@@ -9,5 +9,11 @@ describe GameOfLife do
     it 'returns an array' do
       expect(subject.tick).to be_kind_of Array
     end
+
+    it 'initializes with an array that reads continuously' do
+      @game = GameOfLife.new
+      @game.board_array[0] = "read me"
+      expect(@game.board_array[99 + 1]).to eq("read me")
+    end
   end
 end
