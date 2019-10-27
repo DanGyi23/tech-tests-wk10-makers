ef7c6e9928a2e6f06851f679b29097cb10368807
Dan Gyi <daniel.gyi@gmail.com>
2019-10-27 08:58:56 +0000

    JS grid in JQuery

---
+++ game-of-life/Javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+  for (let k = 0; k < 11; k++) {
+    let $row_container = $("<tr/>", { id: `${k}` })
+  for (let i = 0; i < 10; i++) {
+    
+    $individualcell = $("<td/>", { id: `${k * i}` })
+    $row_container.append($individualcell).appendTo('#entire_board')
+  };
+};
+});
