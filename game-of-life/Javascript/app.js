5aa66afe6859953ad3b88acfe75ab1de4effbd9c
Dan Gyi <daniel.gyi@gmail.com>
2019-11-11 17:36:23 +0000

    increase board size to 100 x 100

--- game-of-life/Javascript/app.js
+++ game-of-life/Javascript/app.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 
-  for (let k = 0; k < 11; k++) {
+  for (let k = 0; k < 100; k++) {
     let $row_container = $("<tr/>", { id: `${k}` })
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     
-    $individualcell = $("<td/>", { id: `${(k * 10) + i}` })
+    $individualcell = $("<td/>", {
+      id: `${(k * 100) + i}`, style: 'border: 3px solid black; padding: 8px' })
     $row_container.append($individualcell).appendTo('#entire_board')
   };
 };
