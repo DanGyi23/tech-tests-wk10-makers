7ca4db3554a859dfd32f7fa04b93ff8cba60080e
Dan Gyi <daniel.gyi@gmail.com>
2019-11-11 20:54:01 +0000

    fully transposed JS version

--- game-of-life/Javascript/logic.js
+++ game-of-life/Javascript/logic.js
@@ -6,28 +6,35 @@ GameOfLife = function() {
       this._board_array.forEach(function (element, index) {
         if (element == 1 && (this.live_neighbour_total(index) == 2 || this.live_neighbour_total(index) == 3)) {
           element = 1
-        } elsif (element == 1 && live_neighbour_total(index) < 2) {
+        } elsif (element == 1 && live_neighbour_total(index) < 2); {
           element = 0
-        } elsif (element == 1 && live_neighbour_total(index) > 3) {
+        } elsif (element == 1 && live_neighbour_total(index) > 3); {
           element = 0
-        } elsif (element == 0 && live_neighbour_total(index) == 3) {
+        } elsif (element == 0 && live_neighbour_total(index) == 3); {
           element = 1
         };
 
       });
+      return this._board_array
+    };
+
+    GameOfLife.prototype.getElementAt = function(index) {
+      corrected_index = this._board_array[(index % this._board_array.length)]
+      return corrected_index
+    };
+
+    GameOfLife.prototype.live_neighbour_total = function(index) {
+      referential_array = [1, -1, 100, -100, 99, -99, 101, -101]
+      
+      referential_array.forEach(function(x) {
+          x = get_element_at((index + x))
+      });
+      return referential_array
     };
 
 };
 
 
-
-
-// def get_element_at(index)
-// index = index % @board_array.length;
-// index = index + @board_array.length;
-// index = index % @board_array.length;
-// return @board_array[index]
-// end
 
 // def live_neighbour_total(current_index)
 // [1, -1, 100, -100, 99, -99, 101, -101].map { | x |
