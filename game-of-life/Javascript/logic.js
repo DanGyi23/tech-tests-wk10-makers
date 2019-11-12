4563c1c17d46704c86eba63f21c32fc28bde5fd9
Dan Gyi <daniel.gyi@gmail.com>
2019-11-12 13:21:20 +0000

    working front end

--- game-of-life/Javascript/logic.js
+++ game-of-life/Javascript/logic.js
@@ -2,7 +2,7 @@ GameOfLife = function() {
     var board_array = []
     this._board_array = board_array
     for (let i = 0; i < 10000; i++ ) {
-      board_array.push(Math.round(Math.random() * 0.7));
+      board_array.push(Math.round(Math.random() * 0.66));
     }
     
     GameOfLife.prototype.tick = function() {
