e6e8400d37362af25708c70223d640085a305352
Dan Gyi <daniel.gyi@gmail.com>
2019-11-26 09:33:47 +0000

    can place ship of length 2 on board

--- battleships/lib/board.rb
+++ battleships/lib/board.rb
@@ -5,4 +5,9 @@ class Board
     rows, cols = 8, 8
     @board = Array.new(rows) { Array.new(cols) }
   end
+
+  def place_ship(ship, start_coords, end_coords)
+    @board[start_coords[0]][start_coords[1]] = ship
+    @board[end_coords[0]][end_coords[1]] = ship
+  end
 end
